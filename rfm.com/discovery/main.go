c7826bd19f1066679275f027cd77ebf85ba98e2f
Augusto Oliveira <augusto.oliveira@confirmationcall.com.br>
2024-11-06 20:13:33 -0300

    remove swagger from discovery

--- rfm.com/discovery/main.go
+++ rfm.com/discovery/main.go
@@ -4,11 +4,9 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	httpSwagger "github.com/swaggo/http-swagger"
 	"github.com/thoas/go-funk"
 	"net/http"
 	"rfm.com/common"
-	_ "rfm.com/discovery/docs"
 	"slices"
 	"strconv"
 	"strings"
@@ -19,20 +17,6 @@ const ClientPort = 9090
 
 var services []Service
 
-//	@title			Remote-File-Manager - Discovery
-//	@version		1.0
-//	@description	This is the discovery service that receive connections from executor and the client
-//	@termsOfService	http://swagger.io/terms/
-
-//	@contact.name	API Support
-//	@contact.url	http://www.swagger.io/support
-//	@contact.email	support@swagger.io
-
-//	@license.name	Apache 2.0
-//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host		localhost:7070
-// @BasePath	/
 func main() {
 
 	fmt.Println("****RUNNING****")
@@ -47,21 +31,10 @@ func listenToServices() {
 	router := http.NewServeMux()
 
 	router.HandleFunc("POST /register", handleRegisterService)
-	router.HandleFunc("GET /swagger/", httpSwagger.WrapHandler)
 	http.ListenAndServe(":"+strconv.Itoa(DiscoveryPort), router)
 
 }
 
-// handleRegisterService
-// @Summary
-// @Description Register a service in the service discovery
-// @ID register-service
-// @Accept json
-// @Produce text/plain
-// @Param request body common.FeatureRegister true "query params"
-// @Success 200 {string} []string "ok"
-// @Failure 500 {object} string "Can not get the directories"
-// @Router /register [post]
 func handleRegisterService(w http.ResponseWriter, r *http.Request) {
 	var featureRegister common.FeatureRegister
 	_ = json.NewDecoder(r.Body).Decode(&featureRegister)
@@ -73,7 +46,6 @@ func handleRegisterService(w http.ResponseWriter, r *http.Request) {
 
 func handleServiceDiscovery(addr string, featureRegister common.FeatureRegister) (int, string) {
 
-	//remove initial '/' from addr
 	featureRegister.Commands = funk.Map(featureRegister.Commands, func(k string, v string) (string, string) {
 
 		return k, strings.TrimPrefix(v, "/")

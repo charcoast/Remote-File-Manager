6a873d38d4ff49e945555980799f1175fe7e788a
Augusto Oliveira <augusto.oliveira@confirmationcall.com.br>
2024-11-26 20:43:27 -0300

    fix cors

--- rfm.com/discovery/main.go
+++ rfm.com/discovery/main.go
@@ -31,7 +31,7 @@ func main() {
 	initDB()
 
 	fmt.Println("****RUNNING****")
-	c = cors.New(cors.Options{AllowedOrigins: []string{"http://http://167.234.232.150"},
+	c = cors.New(cors.Options{AllowedOrigins: []string{"http://167.234.232.150"},
 		AllowCredentials: true,
 		Debug:            true})
 

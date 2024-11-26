d55b50f1a121817746a8cf8819418660260b8e6e
Augusto Oliveira <augusto.oliveira@confirmationcall.com.br>
2024-11-26 20:54:39 -0300

    print err

--- rfm.com/executors/list/api/api.go
+++ rfm.com/executors/list/api/api.go
@@ -56,7 +56,7 @@ func getDirectories(w http.ResponseWriter, path string) {
 	path = common.BasePath + path
 	dirs, err := os.ReadDir(path)
 	if err != nil {
-		print(err)
+		print(err.Error())
 		_ = returnJsonObject(&w, throwNewListException("GetDirectoriesException", "Ocorreu um erro ao consultar os diretórios"))
 	}
 	var directories = make([]model.Entry, 0)
